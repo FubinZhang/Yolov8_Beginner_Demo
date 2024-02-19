d36c8c6bc7da7c359030d49b33850520eca23ab1
FubingZhang <fjfzzzffbb@gmail.com>
2024-02-19 16:48:46 +0800

    Changes to be committed: 	modified:   instance_segmentation/train.py

--- instance_segmentation/train.py
+++ instance_segmentation/train.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 model = YOLO('yolov8n-seg.yaml')  # build a new model from YAML
 model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n-seg.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
-
+#
 # Train the model
 results = model.train(data='./leaf_sets/dataset.yaml', epochs=500, batch=16, imgsz=640, device=0, workers=0, save_period=50)
